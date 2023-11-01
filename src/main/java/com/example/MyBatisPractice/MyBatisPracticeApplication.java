a2bb2c89407f9fb63accfdcca5f4c8b18da28cbe
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-01 17:26:04 +0900

    動的なSQL作成、データの挿入、データの更新

--- src/main/java/com/example/MyBatisPractice/MyBatisPracticeApplication.java
+++ src/main/java/com/example/MyBatisPractice/MyBatisPracticeApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -25,7 +27,65 @@ public class MyBatisPracticeApplication {
 	private void execute() {
 		List<Mansion> list = mansionRepository.selectAll();
 		System.out.println(list);
+
 		Mansion mansion = mansionRepository.selectById(1);
 		System.out.println(mansion);
+
+		mansion = new Mansion();
+		mansion.setStatus(true);
+		list = mansionRepository.selectByStatusBuildingDate(mansion);
+		System.out.println(list);
+
+		mansion = new Mansion();
+		mansion.setStatus(true);
+		mansion.setBuildingDate(Date.valueOf("2010-05-01"));
+		list = mansionRepository.selectByStatusBuildingDate(mansion);
+		System.out.println(list);
+
+		mansion = new Mansion();
+		mansion.setFloorPlanId(1);
+		List<Mansion> result = mansionRepository.selectByFloorPlanIdBuildingDateStatus(mansion);
+		System.out.println(result);
+
+		mansion = new Mansion();
+		mansion.setBuildingDate(Date.valueOf("2010-01-01"));
+		result = mansionRepository.selectByFloorPlanIdBuildingDateStatus(mansion);
+		System.out.println(result);
+
+		mansion = new Mansion();
+		mansionRepository.selectByFloorPlanIdBuildingDateStatus(mansion);
+		System.out.println(result);
+
+		List<Integer> idList= new ArrayList<Integer>();
+		idList.add(1);
+		idList.add(2);
+		idList.add(4);
+		list = mansionRepository.selectByIdIN(idList);
+		System.out.println(list);
+
+		list=mansionRepository.selectByStationLIKE("宮");
+		System.out.println(list);
+
+		mansion = new Mansion();
+		mansion.setName("ローズガーデンハイツ");
+		mansion.setStation("大井町駅");
+		result=mansionRepository.selectByNameStation(mansion);
+		System.out.println(result);
+
+		mansion = new Mansion();
+		mansion.setName("サニーハイツ");
+		mansion.setAddress("東京都品川区東大井ｘｘｘ");
+		mansion.setStation("大井町駅");
+		mansion.setFloorPlanId(1);
+		mansion.setBuildingDate(Date.valueOf("2023-04-01"));
+		mansion.setStatus(true);
+		mansionRepository.insert(mansion);
+
+		mansion = new Mansion();
+		mansion.setId(1);
+		mansion.setName("GreenHills");
+		mansion.setAddress("東京都渋谷区渋谷111");
+		mansion.setStation("JR渋谷駅");
+		mansionRepository.update(mansion);
 	}
 }
--- src/main/java/com/example/MyBatisPractice/repository/MansionRepository.java
+++ src/main/java/com/example/MyBatisPractice/repository/MansionRepository.java
@@ -2,9 +2,21 @@ package com.example.MyBatisPractice.repository;
 
 import com.example.MyBatisPractice.entity.Mansion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface MansionRepository {
     public List<Mansion> selectAll();
     public Mansion selectById(Integer id);
+    public List<Mansion> selectByStatusBuildingDate (Mansion mansion);
+    public List<Mansion> selectByFloorPlanIdBuildingDateStatus (Mansion mansion);
+    public List<Mansion> selectByIdIN(List<Integer> idList);
+
+    public List<Mansion> selectByStationLIKE(String stationName);
+
+    List<Mansion> selectByNameStation(Mansion mansion);
+    Boolean insert(Mansion mansion);
+    Boolean update(Mansion mansion);
+
+
 }
