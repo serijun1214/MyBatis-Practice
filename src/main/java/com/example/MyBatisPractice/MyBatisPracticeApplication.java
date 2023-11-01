9e8b74ff767ad84994da7d82174e224b039ff394
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-01 16:50:33 +0900

    DBからデータを取得する

--- src/main/java/com/example/MyBatisPractice/MyBatisPracticeApplication.java
+++ src/main/java/com/example/MyBatisPractice/MyBatisPracticeApplication.java
@@ -1,13 +1,31 @@
 package com.example.MyBatisPractice;
 
+import com.example.MyBatisPractice.entity.Mansion;
+import com.example.MyBatisPractice.repository.MansionRepository;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
+@MapperScan("com.example.MyBatisPractice.repository")
 public class MyBatisPracticeApplication {
+	@Autowired
+	MansionRepository mansionRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyBatisPracticeApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MyBatisPracticeApplication.class, args);
+		MyBatisPracticeApplication app = context.getBean(MyBatisPracticeApplication.class);
+		app.execute();
 	}
 
+	private void execute() {
+		List<Mansion> list = mansionRepository.selectAll();
+		System.out.println(list);
+		Mansion mansion = mansionRepository.selectById(1);
+		System.out.println(mansion);
+	}
 }
--- src/main/java/com/example/MyBatisPractice/repository/MansionRepository.java
+++ src/main/java/com/example/MyBatisPractice/repository/MansionRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface MansionRepository {
     public List<Mansion> selectAll();
+    public Mansion selectById(Integer id);
 }
