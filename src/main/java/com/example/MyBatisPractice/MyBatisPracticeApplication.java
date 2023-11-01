6988b17a51e749ba828fa73e84141e0161e45864
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-01 11:14:47 +0900

    first commit

---
+++ src/main/java/com/example/MyBatisPractice/MyBatisPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyBatisPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyBatisPractice/MyBatisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyBatisPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
